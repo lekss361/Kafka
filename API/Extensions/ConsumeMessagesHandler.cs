b7b79054fcc958d912d67fabfe21db6f9e1f09f1
Алексей Кондратьев <kondratievaf@ekassir.com>
2022-10-21 17:46:41 +0300

    add debug Log to ConsumeMessagesHandler

--- API/Extensions/ConsumeMessagesHandler.cs
+++ API/Extensions/ConsumeMessagesHandler.cs
@@ -1,15 +1,24 @@
 ﻿using KafkaFlow;
 using System.Diagnostics;
 using KafkaFlow.TypedHandler;
+using API.Controllers;
+using Newtonsoft.Json;
 
 namespace API.Extensions
 {
     public class ConsumeMessagesHandler : IMessageHandler<string>
     {
+        private readonly ILogger<ConsumeMessagesHandler> _logger;
+
+        public ConsumeMessagesHandler(ILogger<ConsumeMessagesHandler> logger)
+        {
+            _logger = logger;
+        }
+
         public Task Handle(IMessageContext context, string message)
         {
             GlobalVariables.responseKafkaMessages.Add(new Model.ResponseKafkaMessagesModel(context.ConsumerContext.Offset, message));
-
+            _logger.LogInformation(JsonConvert.SerializeObject(context));
             Debug.WriteLine(
                 "Partition: {0} | Offset: {1} | Message: {2}",
                 context.ConsumerContext.Partition,
